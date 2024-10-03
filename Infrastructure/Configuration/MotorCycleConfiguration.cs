f9e410f680b784974b272770f4cc3143106fbf25
Otávio Martins Salomão <otaviosalomao@gmail.com>
2024-10-03 13:14:49 -0300

    Added Entity Framework

---
+++ Infrastructure/Configuration/MotorCycleConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ride_wise_api.Domain.Models;
+
+namespace ride_wise_api.Infrastructure.Configuration
+{
+    public class MotorCycleConfiguration : IEntityTypeConfiguration<MotorCycle>
+    {
+        public void Configure(EntityTypeBuilder<MotorCycle> builder)
+        {
+            builder.HasKey(u => new { u.LicensePlate });            
+        }
+    }
+}
---
+++ Infrastructure/RiseWiseManagerDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data;
+using System.Reflection;
+using System;
+using ride_wise_api.Domain.Models;
+
+namespace ride_wise_api.Infrastructure
+{
+    public class RiseWiseManagerDbContext : DbContext
+    {
+        public DbSet<DeliveryAgent> DeliveryAgents { get; set; }
+        public DbSet<MotorCycle> MotorCycles { get; set; }
+        public DbSet<Rental> Rentals { get; set; }
+
+        public RiseWiseManagerDbContext(DbContextOptions<RiseWiseManagerDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
